3322d1c361a16d7b283bb2492cb9e965a71eac02
Victor Stepanov <vix522430@bk.ru>
2023-01-24 20:20:52 +0300

    feat:added e2e stack testing

--- cypress/const/const.ts
+++ cypress/const/const.ts
@@ -1,3 +1,4 @@
 export const CIRCLE_CIRCLE = "[class*=circle_circle]";
+export const CIRCLE_DEFAULT = "4px solid rgb(0, 50, 255)";
 export const CIRCLE_MODIFIED = "4px solid rgb(127, 224, 81)";
 export const CIRCLE_CHANGING = "4px solid rgb(210, 82, 225)";
