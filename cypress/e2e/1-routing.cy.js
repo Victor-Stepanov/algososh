a34ade309c789e233fe7f325f7d5640bab4c5a86
Victor Stepanov <vix522430@bk.ru>
2023-01-25 16:56:13 +0300

    fix

--- cypress/e2e/1-routing.cy.js
+++ cypress/e2e/1-routing.cy.js
@@ -1,13 +1,31 @@
-describe('Тестирование переходов по страницам', () => [
+describe('Тестирование переходов по страницам', () => {
   beforeEach('проверка работоспособности сайта', () => {
     cy.visit('/');
-  }),
-  it('проверка переходов', () => {
-    cy.visit('/recursion');
-    cy.visit('/fibonacci');
-    cy.visit('/sorting');
-    cy.visit('/stack');
-    cy.visit('/queue');
-    cy.visit('/list');
-  }),
-]);
+  })
+  describe('проверка переходов:', () => {
+    it('по адресу localhost:3000/recursion', () => {
+      cy.visit('/recursion');
+      cy.contains('Строка')
+    })
+    it('по адресу localhost:3000/fibonacci', () => {
+      cy.visit('/fibonacci');
+      cy.contains('Последовательность Фибоначчи')
+    })
+    it('по адресу localhost:3000/sorting', () => {
+      cy.visit('/sorting');
+      cy.contains('Сортировка массива')
+    })
+    it('по адресу localhost:3000/stack', () => {
+      cy.visit('/stack');
+      cy.contains('Стек')
+    })
+    it('по адресу localhost:3000/queue', () => {
+      cy.visit('/queue');
+      cy.contains('Очередь')
+    })
+    it('по адресу localhost:3000/list', () => {
+      cy.visit('/list');
+      cy.contains('Связный список')
+    })
+  })
+});
